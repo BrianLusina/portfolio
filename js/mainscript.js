a8946b1510f8b0bb8cd07bd1a7c7b025c8c92f01
Brian Lusina <lusinabrian@gmail.com>
2016-07-20 17:44:01 +0300

    Update site styles

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
-	/*ADD bounce effect to social icons*/
-
+	/*change color of social links
+	Store social links in variable
+	*/
+	var social = $('.social-links i');
+	function changeColor(){
+		//perform class checks
+		//if twitter, change to blue
+		if($(this).hasClass('fa-twitter')){
+			$(this).css('color','#00ABF1');
+			/*else if linkedin*/
+		}else if($(this).hasClass('fa-linkedin')){
+			$(this).css('color','#0177B5');
+		}else if($(this).hasClass('fa-github')){
+			$(this).css('color','#000');			
+		}else if($(this).hasClass('fa-medium')){
+			$(this).css('color','#00AB6B');
+		}else if($(this).hasClass('fa-google-plus')){
+			$(this).css('color','#DB4437');
+		}else if($(this).hasClass('fa-codepen')){
+			$(this).css('color','#878787');
+		}		
+	}
+	
+	social.on('mouseenter',changeColor);
+	social.on('mouseleave', function(){
+		$(this).css('color','white');
+	})
 });
+

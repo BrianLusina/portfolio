25843a7484d349078b433390ad826ea1b6a52f68
BrianLusina <lusinabrian@gmail.com>
2016-10-16 11:00:28 +0300

    Update contact form, add gmail api

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,46 @@
 $(document).ready(function(){
+	var clientId = "737769653941-65s87kmhafi7cb109248pta69em9j8qh.apps.googleusercontent.com";
+    var apiKey = "Xkmhzbq-o6xvX5x0zodEmlh5";
+    var scopes = "https://www.googleapis.com/auth/gmail.send";
+
+    function sendEmail() {
+        $('#email-me-btn');
+        sendMessage({
+                'To': "lusinabrian@gmail.com",
+                "FromName":$('#contact-name').val(),
+                "FromEmail":$('#contact-email').val()
+            },
+            $('#contact-message').val(),
+            composeTidy
+        );
+        return false;
+    }
+    /*This function is where we interact with the Gmail API. It accepts an object of email headers, the email body and a callback function.*/
+    function sendMessage(headers_obj, message, callback) {
+        var email = '';
+
+        for(var header in headers_obj)
+            email += header += ": "+headers_obj[header]+"\r\n";
+
+        email += "\r\n" + message;
+
+        var sendRequest = gapi.client.gmail.users.messages.send({
+            'userId': 'me',
+            'resource': {
+                'raw': window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
+            }
+        });
+
+        return sendRequest.execute(callback);
+    }
+
+    /*clear all the fields*/
+    function composeTidy() {
+        $('#contact-name').val("");
+        $('#contact-email').val("");
+        $('#contact-message').val("");
+    }
+
 	/*change color of social links
 	Store social links in variable
 	*/

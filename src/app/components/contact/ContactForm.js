62970b6a670077a484f3d88a3409914564387f4e
Brian Lusina <lusinabrian@gmail.com>
2017-10-08 18:07:09 +0300

    Using inputChange Handler for all inputs
    this will be used to handle the input types in the form and update the state of the component with data from the form. This will pipe all
    the form state to the react component state and be able to keep the component aware of the internal state of the form. Note that this will
    also be usefull when updating the state of the form inputs, as they are being updated from the internal state of the component.
    Using 1 handler function to handle input changes, ensures that the application component is clearer and more terse and also allows for using
    ES6 Computed Property Name to handle the changes of the component state.

--- src/app/components/contact/ContactForm.js
+++ src/app/components/contact/ContactForm.js
@@ -1,34 +1,32 @@
 /**
  * @author lusinabrian on 30/09/17.
- * @notes: Contact form
+ * @notes: Contact form component
  */
 
 import React, {Component} from 'react';
-import { sendFormDataApi } from "../../api/theLusinaApi"
+import {sendFormDataApi} from "../../api/theLusinaApi"
 
 export default class ContactForm extends Component {
     constructor(props, context) {
         super(props, context);
 
         this.state = {
-            senderEmail :"",
-            senderName : "",
-            senderMessage: ""
+            email: "",
+            name: "",
+            message: ""
         };
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handleMessageChange = this.handleMessageChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     /**
      * Used to send form data
      * @param event event object obtained from DOM element*/
-    handleSubmit(event){
-        event.preventDefault();
+    handleSubmit(event) {
         let senderData = {
-            senderName: this.state.senderName,
-            senderEmail: this.state.senderEmail,
-            senderMessage: this.state.senderMessage
+            name: this.state.name,
+            email: this.state.email,
+            message: this.state.message
         };
 
         sendFormDataApi(senderData).then(() => {
@@ -37,28 +35,25 @@ export default class ContactForm extends Component {
         }).catch(error => {
             // reject
 
-        })
+        });
+        event.preventDefault();
     }
 
     /**
-     * Handles name change for the given form
+     * Handles input changes in contact form
+     * This will handle both email and name input fields
+     * Using ES6 Computed Property name to handle updating the state, the name is the key
+     * and the value is the `value` this will come from the event object
+     * @param {object} event Event object with data from the input fields
      * */
-    handleNameChange(event){
+    handleInputChange(event){
+       const target = event.target;
+       const value = target.type === 'email' ? target.value : target.value;
+       const name = target.name;
 
-    }
-
-    /**
-     * Handles email change in email input area
-     * */
-    handleEmailChange(event){
-
-    }
-
-    /**
-     * Handles message change in text area
-     * */
-    handleMessageChange(event){
-
+       this.setState({
+           [name]: value
+       });
     }
 
     render() {
@@ -66,58 +61,61 @@ export default class ContactForm extends Component {
             <form id="contact-form" method="POST" onSubmit={this.handleSubmit}>
                 <div className="row">
                     <div className="small-6 columns">
-                                <span className="input input--kaede">
-                                    <input className="input__field input__field--kaede" type="text" name="name"
-                                           id="contact-name" required
-                                           value={this.state.senderName}
-                                           onChange={this.handleNameChange}/>
-                                    <label className="input__label input__label--kaede" htmlFor="contact-name">
-                                        <span className="input__label-content input__label-content--kaede">Name</span>
-									</label>
-								</span>
+                        <span className="input input--kaede">
+                            <input className="input__field input__field--kaede" type="text"
+                                   name="name" id="contact-name" required
+                                   value={this.state.mame}
+                                   onChange={this.handleInputChange}/>
+                                <label className="input__label input__label--kaede"
+                                       htmlFor="contact-name">
+                                    <span className="input__label-content
+                                     input__label-content--kaede">Name</span>
+                                </label>
+                        </span>
                     </div>
 
                     <div className="small-6 columns">
-								<span className="input input--kaede">
-									<input className="input__field input__field--kaede" type="email" name="email"
-                                           id="contact-email" required
-                                    value={this.state.senderEmail}
-                                    onChange={this.handleEmailChange}/>
-									<label className="input__label input__label--kaede" htmlFor="contact-email">
-										<span className="input__label-content input__label-content--kaede">Email</span>
-									</label>
-								</span>
+                        <span className="input input--kaede">
+                            <input className="input__field input__field--kaede"
+                                   type="email" name="email" id="contact-email" required
+                                   value={this.state.email}
+                                   onChange={this.handleInputChange}/>
+                            <label className="input__label input__label--kaede" htmlFor="contact-email">
+                                <span className="input__label-content
+                                input__label-content--kaede">Email</span>
+                            </label>
+                        </span>
                     </div>
                 </div>
 
                 <div className="row">
                     <div className="small-12 columns">
-							<span className="input input--kaede" style={{height:"150px"}}>
-								<textarea className="input__field input__field--kaede"
-                                          type="text" name="message"
-                                          id="contact-message"
-                                          required
-                                value={this.state.senderMessage}
-                                onChange={this.handleMessageChange}/>
-								<label className="input__label input__label--kaede"
-                                       htmlFor="contact-message">
-									<span className="input__label-content input__label-content--kaede">Message</span>
-								</label>
-							</span>
+                        <span className="input input--kaede" style={{height: "150px"}}>
+                            <textarea className="input__field input__field--kaede"
+                                      type="text" name="message" id="contact-message" required
+                                      value={this.state.message}
+                                      onChange={this.handleInputChange}/>
+                            <label className="input__label input__label--kaede"
+                                   htmlFor="contact-message">
+                                <span className="input__label-content
+                                input__label-content--kaede">Message</span>
+                            </label>
+                        </span>
                     </div>
                 </div>
 
                 <div className="row">
                     <div className="small-12 columns">
                         <div type="submit" id="email-btn-container">
-                            <button type="submit" id="email-me-btn" className="large button animated"
-                                    data-text="EMAIL ME">
+                            <button type="submit" id="email-me-btn"
+                                    className="large button animated" data-text="EMAIL ME">
                                 <span>S</span><span>E</span><span>N</span><span>D</span>
                                 <span>M</span><span>A</span><span>I</span><span>L</span>
                             </button>
                         </div>
                     </div>
                 </div>
+
             </form>
         );
     }

0eb1b1e12ae784cfac8100ba6c40cda804c8dd77
Brian Lusina <lusinabrian@gmail.com>
2017-10-08 15:40:00 +0300

    add more test cases to about

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -25,11 +25,39 @@ describe("About should", () => {
         render(<wrapper/>, document.createElement("div"));
     });
 
-    // TODO: adjust this test case based on your root tag    
-    it("contain 1 div element", () => {
-
+    it("contain 1 section element", () => {
         expect(wrapper.find("section").length).toBe(1);
     });
 
-    // TODO: other test cases
+    it("contain 4 div element", () => {
+        expect(wrapper.find("div").length).toBe(5);
+    });
+
+    it("contain h3 element with title `A little bit about me.`", () => {
+        expect(wrapper.find("h3").length).toBe(1);
+        expect(wrapper.find("h3").text()).toEqual("A little bit about me.");
+    });
+
+    it("contain 6 a elements", () => {
+        expect(wrapper.find("a").length).toBe(7);
+    });
+
+    it("have first anchor tag to have link to portfolio", () => {
+        expect(wrapper.find("a").at(0).props().href).toEqual("http://lusinaresume.netlify.com/");
+    });
+
+    it("contain second to last anchor tags with class social links and child i", () => {
+        for(let i = 1 ;i < wrapper.find("a").length; i++){
+            expect(wrapper.find("a").at(i).hasClass("social-links")).toBe(true);
+            expect(wrapper.find("a").at(i).props().children).toBeDefined();
+        }
+    });
+
+    it("contain h4 header call to action with id connect-about and text Feel free to connect ", () => {
+        expect(wrapper.find("h4").length).toEqual(1);
+        expect(wrapper.find("h4").props().id).toEqual("connect-about");
+        expect(wrapper.find("h4").text()).toEqual("Feel free to connect.");
+    });
+
+
 });

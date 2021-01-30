c304223d6a3c995abb73b968ad9b29fb5797f370
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-01-30 19:47:00 +0300

    refactor(project): initial project setup
    Initial project setup for React revamp

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="cd-header">
+        <a href="/index.html" className="cd-logo rolling-links">
+          <span data-hover="A Designer">The Lusina</span></a>
+        <a href="#0" className="cd-3d-nav-trigger">
+          Menu
+          <span></span>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

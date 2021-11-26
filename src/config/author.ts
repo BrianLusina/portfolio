824bfcd90e69a977af0a557beae66e21cfacd946
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-11-26 09:12:01 +0300

    chore(config): configuration
    Addition of author config

---
+++ src/config/author.ts
@@ -0,0 +1,5 @@
+export default {
+    name: process.env.AUTHOR_NAME || '@Lusina',
+    linkedIn: process.env.AUTHOR_LINKEDIN || 'https://www.linkedin.com/in/brianlusina/',
+    githubRepos: process.env.AUTHOR_GITHUB || 'https://github.com/BrianLusina?tab=repositories',
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,12 @@
 import api from './api';
 import menuItems from './menu';
+import author from './author';
 
 export default {
   api,
   name: process.env.NAME || 'Projects',
   title: process.env.TITLE || 'Liber',
   owner: process.env.OWNER || 'BrianLusina',
-  menuItems: menuItems
+  author,
+  menuItems
 };

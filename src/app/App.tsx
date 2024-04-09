96ce79f9a06318202c17edffab27ac48a20ed4a7
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-04-09 21:48:00 +0300

    feat(components): scroll to top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
 import { Suspense, FunctionComponent, lazy } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { withProfiler } from '@sentry/react';
+import { FiChevronUp } from "react-icons/fi";
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import config from '@config';
 import RouteErrorBoundary from '@components/Errors/RouteErrorBoundary';
 import MainLayout from '@layouts/MainLayout';
 import Footer from '@components/Footer';
 import PageLoader from '@components/Elements/Loaders/PageLoader';
-import ScrollToTop from '@components/ScrollToTop';
+import { WindowScrollToTop, ScrollToTopButton } from '@components/ScrollToTop';
 import SocialCard from '@components/SocialCard';
 import usePageViews from '@hooks/analytics/usePageView';
 
@@ -24,7 +25,7 @@ const App: FunctionComponent = () => {
     <>
       <AppWrapper id="wrapper">
         <MainLayout>
-          <ScrollToTop />
+          <WindowScrollToTop />
           <Suspense fallback={<PageLoader />}>
             <TransitionGroup>
               <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
@@ -53,8 +54,15 @@ const App: FunctionComponent = () => {
         <Footer>
           <SocialCard />
         </Footer>
+
+        {/* Start Back To Top */}
+        <div className="backto-top">
+          <ScrollToTopButton showUnder={160}>
+              <FiChevronUp />
+          </ScrollToTopButton>
+        </div>
+
       </AppWrapper>
-      {/* <Menu items={config.menuItems} /> */}
     </>
   );
 };
